d18bc2e51c00fd4767c5d896ce007a8b1d9cea16
ericsaucedo <eric@setandforget.io>
2023-04-13 08:58:41 -0300

    Fixing mappings

--- components/TableEdit.tsx
+++ components/TableEdit.tsx
@@ -80,78 +80,90 @@ export default function Example({ pnls, handleChange, handleChangeText, handleCh
             </tr>
           </thead>
           <tbody>
-            {pnls.map((pnl: any, planIdx: number) => (
-              <tr key={pnl.name}>
-                <td
-                  className={classNames(
-                    planIdx === 0 ? '' : 'border-t border-transparent',
-                    'relative py-4 pl-4 pr-3 text-sm sm:pl-6'
-                  )}
-                >
-                  <div className="font-medium text-gray-900">
-                    {pnl.name}
-                    {/* {plan.isCurrent ? <span className="ml-1 text-indigo-600">(Current Plan)</span> : null} */}
-                  </div>
-                  {/* <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
-                    <span>
-                      {plan.memory} / {plan.cpu}
-                    </span>
-                    <span className="hidden sm:inline">·</span>
-                    <span>{plan.storage}</span>
-                  </div> */}
-                  {planIdx !== 0 ? <div className="absolute right-0 left-6 -top-px h-px bg-gray-200" /> : null}
-                </td>
-                <td
-                  className={classNames(
-                    planIdx === 0 ? '' : 'border-t border-gray-200',
-                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
-                  )}
-                >
-                  {pnl.group}
-                </td>
-                <td
-                  className={classNames(
-                    planIdx === 0 ? '' : 'border-t border-gray-200',
-                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
-                  )}
-                >
-                  <Switch
-                    checked={pnl.isIncluded}
-                    onChange={() => handleChange(planIdx)}
+            {pnls.map((pnl: any, planIdx: number) => {
+              if (
+                pnl.name !== 'Total Income' && 
+                pnl.name !== 'Gross profit' && 
+                pnl.name !== 'Total Expenses' &&
+                pnl.name !== 'Net Operating Income' &&
+                pnl.name !== 'Net Income' &&
+                pnl.name !== 'Cost of Goods Sold' &&
+                pnl.name !== 'Total Cost of Goods Sold' &&
+                pnl.name !== 'Total Other Expenses'
+              )
+              return (
+                <tr key={pnl.name}>
+                  <td
                     className={classNames(
-                      pnl.isIncluded ? 'bg-indigo-600' : 'bg-gray-200',
-                      'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      planIdx === 0 ? '' : 'border-t border-transparent',
+                      'relative py-4 pl-4 pr-3 text-sm sm:pl-6'
                     )}
                   >
-                    <span
-                      aria-hidden="true"
+                    <div className="font-medium text-gray-900">
+                      {pnl.name}
+                      {/* {plan.isCurrent ? <span className="ml-1 text-indigo-600">(Current Plan)</span> : null} */}
+                    </div>
+                    {/* <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+                      <span>
+                        {plan.memory} / {plan.cpu}
+                      </span>
+                      <span className="hidden sm:inline">·</span>
+                      <span>{plan.storage}</span>
+                    </div> */}
+                    {planIdx !== 0 ? <div className="absolute right-0 left-6 -top-px h-px bg-gray-200" /> : null}
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? '' : 'border-t border-gray-200',
+                      'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                    )}
+                  >
+                    {pnl.group}
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? '' : 'border-t border-gray-200',
+                      'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                    )}
+                  >
+                    <Switch
+                      checked={pnl.isIncluded}
+                      onChange={() => handleChange(planIdx)}
                       className={classNames(
-                        pnl.isIncluded ? 'translate-x-5' : 'translate-x-0',
-                        'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                        pnl.isIncluded ? 'bg-indigo-600' : 'bg-gray-200',
+                        'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                       )}
+                    >
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          pnl.isIncluded ? 'translate-x-5' : 'translate-x-0',
+                          'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                        )}
+                      />
+                    </Switch>
+                    
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? '' : 'border-t border-gray-200',
+                      'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                    )}
+                  >
+                    <input
+                      type="text"
+                      name={pnl.name}
+                      // id="email"
+                      value={pnl.nameOnTemplate}
+                      onChange={event => handleChangeText(planIdx, event)}
+                      disabled={!pnl.isIncluded}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="Text"
                     />
-                  </Switch>
-                  
-                </td>
-                <td
-                  className={classNames(
-                    planIdx === 0 ? '' : 'border-t border-gray-200',
-                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
-                  )}
-                >
-                  <input
-                    type="text"
-                    name={pnl.name}
-                    // id="email"
-                    value={pnl.nameOnTemplate}
-                    onChange={event => handleChangeText(planIdx, event)}
-                    disabled={!pnl.isIncluded}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="Text"
-                  />
-                </td>
-              </tr>
-            ))}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
