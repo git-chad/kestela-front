fa791af29e26be87180630add4cc7b6b4aef0309
ericsaucedo <eric@setandforget.io>
2023-04-13 06:54:22 -0300

    Fix: Typescript

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -1,22 +1,22 @@
 import Sidebar from './Sidebar';
-import { useSession } from 'next-auth/react'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useSession } from 'next-auth/react'
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session } = useSession()
-  const router = useRouter()
-  useEffect(() => {
-    if (session?.error === 'RefreshAccessTokenError') {
-      console.log(session?.error);
+  // const { data: session } = useSession()
+  // const router = useRouter()
+  // useEffect(() => {
+  //   if (session?.error === 'RefreshAccessTokenError') {
+  //     console.log(session?.error);
       
-      // router.push("/login")
-    }
-  }, [session])
+  //     // router.push("/login")
+  //   }
+  // }, [session])
 
   return (
     <div className="grid grid-cols-6 grid-rows-1 min-h-screen max-h-screen">
