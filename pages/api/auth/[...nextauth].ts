291bca01614d5b583c60c7892e6196816f79051c
git-chad <tobimocc@gmail.com>
2023-12-28 12:11:23 -0300

    fix(dev): adapted endpoints to fetch correct backend version

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -81,7 +81,7 @@ const authOptions: AuthOptions = {
           firstname: todo.profile.given_name,
           lastname: todo.profile.family_name
         }
-        const res = await fetch(`${process.env.BACK_URL}/v1/users/register`, {
+        const res = await fetch(`${process.env.BACK_URL}/v2/users/register`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(body),
--- services/qbServices.ts
+++ services/qbServices.ts
@@ -4,7 +4,7 @@ const baseURL = process.env.BACK_URL as string;
 const axios = ApiClient(baseURL);
 
 export async function getQbUri() {
-  const url = '/v1/intuit/authUri';
+  const url = '/v2/intuit/authUri';
   const response = await axios.get(url);
   return response?.data;
 }
@@ -14,56 +14,56 @@ export async function getQbTooken(
   state: string | string[],
   realmId: string | string[]
 ): Promise<QbToken> {
-  const url = `/v1/intuit/integrations?code=${code}&state=${state}&realmId=${realmId}`;
+  const url = `/v2/intuit/integrations?code=${code}&state=${state}&realmId=${realmId}`;
   const response = await axios.get(url);
   return response?.data;
 }
 
 export async function getQbCompanyInfo() {
-  const url = '/v1/intuit/getCompanyInfo';
+  const url = '/v2/intuit/getCompanyInfo';
   const response = await axios.get(url);
   return response?.data;
 }
 
 export async function saveCompanyData(body: PostCompanyBody) {
-  const url = '/v1/quickbooks/company';
+  const url = '/v2/quickbooks/company';
   const response = await axios.post(url, body);
   return response?.data;
 }
 
 export async function getQbCompany(user_id: string | null | undefined) {
-  const url = '/v1/quickbooks/company/' + user_id;
+  const url = '/v2/quickbooks/company/' + user_id;
   const response = await axios.get(url);
   console.log(response)
   return response?.data;
 }
 
 export async function setPnL(user_id: any, company_id: string) {
-  const url = '/v1/quickbooks/setProfitAndLoss';
+  const url = '/v2/quickbooks/setProfitAndLoss';
   const response = await axios.post(url, { user_id, company_id });
   return response?.data;
 }
 
 export async function getPnL(user_id: any) {
-  const url = `/v1/quickbooks/getProfitAndLoss?user_id=${user_id}`;
+  const url = `/v2/quickbooks/getProfitAndLoss?user_id=${user_id}`;
   const response = await axios.get(url)
   return response?.data
 }
 
 export async function saveOneMapping(body: any) {
-  const url = `/v1/quickbooks/mapping`
+  const url = `/v2/quickbooks/mapping`
   const response = await axios.post(url, body)
   return response?.data
 }
 
 export async function saveTemplate(body: any) {
-  const url = `/v1/quickbooks/myTemplates`
+  const url = `/v2/quickbooks/myTemplates`
   const response = await axios.post(url, body)
   return response?.data
 }
 
 export async function getTemplates() {
-  const url = `/v1/quickbooks/myTemplates`
+  const url = `/v2/quickbooks/myTemplates`
   const response = await axios.get(url)
   return response?.data
 }
